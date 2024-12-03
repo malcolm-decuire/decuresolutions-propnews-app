3c9cb006830ddc11bf7482467859353597f80146
malcolm-decuire <malcolm.decuire@gmail.com>
2024-12-03 18:38:56 -0500

    CHG from PN to DSPN for DCR & FINTECH Purposes

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/malcolmdecuire/Desktop/DECURE-SOLUTIONS-CATALOG-CODEBASE/_CODE_2024-20XX/decuresolutions-propnews-app/src/app/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\",\"static/media/f963670c6017484a-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
